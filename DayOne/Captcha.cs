b4edc42860005b4f6226fa14bf7b7f4555783434
Lars Richter <github@n-develop.com>
2017-12-02 16:33:47 +0100

    Day 1 and Day 2

---
+++ DayOne/Captcha.cs
@@ -0,0 +1,61 @@
+﻿namespace DayOne
+{
+    public class Captcha
+    {
+        public int Solve(string input)
+        {
+            int sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int currentDigit = -1;
+                int nextDigit = -1;
+                if (int.TryParse(input[i].ToString(), out currentDigit))
+                {
+                    var nextIndex = i + 1;
+
+                    if (i == input.Length - 1)
+                    {
+                        nextIndex = 0;
+                    }
+
+                    if (int.TryParse(input[nextIndex].ToString(), out nextDigit) && currentDigit == nextDigit)
+                    {
+                        sum += currentDigit;
+                    }
+                }
+            }
+            return sum;
+        }
+
+        public int SolveStageTwo(string input)
+        {
+            int sum = 0;
+            int indexSteps = input.Length / 2;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int currentDigit = -1;
+                int nextDigit = -1;
+                if (int.TryParse(input[i].ToString(), out currentDigit))
+                {
+                    var nextIndex = GetNextIndex(i, indexSteps, input.Length - 1);
+
+                    if (int.TryParse(input[nextIndex].ToString(), out nextDigit) && currentDigit == nextDigit)
+                    {
+                        sum += currentDigit;
+                    }
+                }
+            }
+            return sum;
+        }
+
+        private static int GetNextIndex(int i, int steps, int lastIndex)
+        {
+            if (i < steps)
+            {
+                return i + steps;
+            }
+
+            return (i + steps) - (lastIndex + 1);
+        }
+    }
+}
---
+++ DayTwo/Spreadsheet.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DayTwo
+{
+    public class Spreadsheet
+    {
+        public int GetChecksum(string input)
+        {
+            var sum = 0;
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var line in lines)
+            {
+                var intries = CastToIntegers(line);
+                sum += intries.Max() - intries.Min();
+            }
+            return sum;
+        }
+
+        public int SecondStageChecksum(string input)
+        {
+            var sum = 0;
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var line in lines)
+            {
+                var entries = CastToIntegers(line);
+
+                for (int i = 0; i < entries.Count; i++)
+                {
+                    var others = GetEntriesWithoutIndex(entries, i);
+
+                    var divisible = others.FirstOrDefault(other => (other % entries[i] == 0) || (entries[i] % other == 0));
+
+                    if (divisible != 0)
+                    {
+                        sum += divisible > entries[i] ? divisible / entries[i] : entries[i] / divisible;
+                        break;
+                    }
+
+                }
+            }
+            return sum;
+        }
+
+        private static List<int> GetEntriesWithoutIndex(List<int> entries, int i)
+        {
+            var others = entries.GetRange(0, entries.Count);
+            others.RemoveAt(i);
+            return others;
+        }
+
+        private static List<int> CastToIntegers(string line)
+        {
+            List<int> intries = new List<int>();
+            var entries = line.Split('	', StringSplitOptions.RemoveEmptyEntries);
+            foreach (var entry in entries)
+            {
+                if (int.TryParse(entry, out var number))
+                {
+                    intries.Add(number);
+                }
+            }
+            return intries;
+        }
+    }
+}
