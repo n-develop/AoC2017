31b49547c06a9ba4c68fe1c0b128299f57f67df6
Lars Richter <github@n-develop.com>
2017-12-03 20:57:46 +0100

    Day Two solved. Refactoring needed.

--- AdventOfCode.Tests/DayThree/SpiralTests.cs
+++ AdventOfCode.Tests/DayThree/SpiralTests.cs
@@ -1,13 +1,94 @@
+using AdventOfCode.DayThree;
 using Xunit;
 
-namespace AdventOfCode.Tests
+namespace AdventOfCode.Tests.DayThree
 {
     public class SpiralTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(368078, 607)]
+        [InlineData(4, 3)]
+        [InlineData(13, 5)]
+        [InlineData(17, 5)]
+        [InlineData(23, 5)]
+        public void DetermineQuareSize(int value, int expectedSize)
         {
+            var spiral = new Spiral();
 
+            var size = spiral.DetermineSize(value);
+
+            Assert.Equal(expectedSize, size);
+        }
+
+        [Fact]
+        public void BuildMatrix()
+        {
+            var spiral = new Spiral();
+            spiral.DetermineSize(24);
+            var matrix = new int[,]
+            {
+                {17, 16, 15, 14, 13},
+                {18, 5, 4, 3, 12},
+                {19, 6, 1, 2, 11},
+                {20, 7, 8, 9, 10},
+                {21, 22, 23, 24, 25}
+            };
+
+            spiral.BuildMatrix();
+
+            Assert.Equal(matrix, spiral.GetMatrix());
+        }
+
+        [Theory]
+        [InlineData(12, 3)]
+        [InlineData(23, 2)]
+        [InlineData(1, 0)]
+        public void FindDistance(int target, int expectedDistance)
+        {
+            var spiral = new Spiral();
+            spiral.DetermineSize(25);
+            spiral.BuildMatrix();
+
+            var distance = spiral.FindDistance(target);
+
+            Assert.Equal(expectedDistance, distance);
+        }
+
+        [Fact]
+        public void RealInput()
+        {
+            var spiral = new Spiral();
+            spiral.DetermineSize(368078);
+            spiral.BuildMatrix();
+
+            var distance = spiral.FindDistance(368078);
+
+            Assert.Equal(371, distance);
+        }
+
+        [Theory]
+        [InlineData(6, 10)]
+        [InlineData(133, 133)]
+        [InlineData(27, 54)]
+        public void SecondStage(int input, int output)
+        {
+            var spiral = new Spiral();
+            spiral.DetermineSize(199);
+
+            var nextValue = spiral.FindValueSecondStage(input);
+
+            Assert.Equal(output, nextValue);
+        }
+
+        [Fact]
+        public void RealInputStageTwo()
+        {
+            var spiral = new Spiral();
+            spiral.DetermineSize(368078);
+
+            var nextValue = spiral.FindValueSecondStage(368078);
+
+            Assert.Equal(369601, nextValue);
         }
     }
 }
---
+++ AdventOrCode/DayThree/Spiral.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Drawing;
+
+namespace AdventOfCode.DayThree
+{
+    public class Spiral
+    {
+        private int[,] _matrix;
+        private int _size;
+
+        public int DetermineSize(int maxValue)
+        {
+            var squareRoot = Math.Sqrt(maxValue);
+            var intSqrt = (int)squareRoot;
+            if (intSqrt < squareRoot)
+            {
+                intSqrt++;
+            }
+            _size = intSqrt % 2 == 0 ? intSqrt + 1 : intSqrt;
+            return _size;
+        }
+
+        public void BuildMatrix()
+        {
+            _matrix = new int[_size, _size];
+            var y = (_size / 2);
+            var x = y;
+            var value = 1;
+
+            _matrix[y, x] = value++;
+            _matrix[y, ++x] = value++;
+
+            var currentSquareSize = 3;
+            while (currentSquareSize <= _size)
+            {
+                for (int i = 0; i < currentSquareSize - 2; i++)
+                {
+                    _matrix[--y, x] = value++;
+                }
+
+                for (int i = 0; i < currentSquareSize - 1; i++)
+                {
+                    _matrix[y, --x] = value++;
+                }
+
+                for (int i = 0; i < currentSquareSize - 1; i++)
+                {
+                    _matrix[++y, x] = value++;
+                }
+                for (int i = 0; i < currentSquareSize; i++)
+                {
+                    if (x + 1 >= _size)
+                    {
+                        break;
+                    }
+                    _matrix[y, ++x] = value++;
+                }
+
+                currentSquareSize += 2;
+            }
+        }
+
+        public int[,] GetMatrix()
+        {
+            return _matrix;
+        }
+
+        public int FindDistance(int target)
+        {
+            var center = _size / 2;
+            var targetValue = FindTargetValue(target);
+
+            return Math.Abs(center - targetValue.Y) + Math.Abs(center - targetValue.X);
+        }
+
+        private Point FindTargetValue(int target)
+        {
+            var targetValue = Point.Empty;
+
+            for (int row = 0; row < _matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < _matrix.GetLength(1); col++)
+                {
+                    if (_matrix[row, col] == target)
+                    {
+                        targetValue.X = col;
+                        targetValue.Y = row;
+                        return targetValue;
+                    }
+                }
+            }
+            return targetValue;
+        }
+
+        public int FindValueSecondStage(int input)
+        {
+            _matrix = new int[_size, _size];
+            var y = _size / 2;
+            var x = y;
+
+            _matrix[y, x] = 1;
+            ++x;
+            _matrix[y, x] = GetValueFor(x, y);
+
+            var currentSquareSize = 3;
+            while (currentSquareSize <= _size)
+            {
+                for (int i = 0; i < currentSquareSize - 2; i++)
+                {
+                    --y;
+                    _matrix[y, x] = GetValueFor(x, y);
+                    if (_matrix[y, x] >= input)
+                    {
+                        return _matrix[y, x];
+                    }
+                }
+
+                for (int i = 0; i < currentSquareSize - 1; i++)
+                {
+                    --x;
+                    _matrix[y, x] = GetValueFor(x, y);
+                    if (_matrix[y, x] >= input)
+                    {
+                        return _matrix[y, x];
+                    }
+                }
+
+                for (int i = 0; i < currentSquareSize - 1; i++)
+                {
+                    ++y;
+                    _matrix[y, x] = GetValueFor(x, y);
+                    if (_matrix[y, x] >= input)
+                    {
+                        return _matrix[y, x];
+                    }
+                }
+                for (int i = 0; i < currentSquareSize; i++)
+                {
+                    if (x + 1 >= _size)
+                    {
+                        break;
+                    }
+                    ++x;
+                    _matrix[y, x] = GetValueFor(x, y);
+                    if (_matrix[y, x] >= input)
+                    {
+                        return _matrix[y, x];
+                    }
+                }
+
+                currentSquareSize += 2;
+            }
+            return -1;
+        }
+
+        private int GetValueFor(int x, int y)
+        {
+            int sum = 0;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (IsOutOfBounce(y + i, x + j))
+                    {
+                        continue;
+                    }
+                    sum += _matrix[y + i, x + j];
+                }
+            }
+            return sum;
+        }
+
+        private bool IsOutOfBounce(int y, int x)
+        {
+            return y < 0 || x < 0 || x >= _size || y >= _size;
+        }
+
+    }
+}
