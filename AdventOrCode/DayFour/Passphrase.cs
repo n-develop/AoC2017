ce15cba7a96acefff452f8e9bf6f27cb3494dbd8
Lars Richter <github@n-develop.com>
2017-12-04 10:18:45 +0100

    Added day four

---
+++ AdventOrCode/DayFour/Passphrase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.DayFour
+{
+    public class Passphrase
+    {
+        private readonly string _phrase;
+
+        public Passphrase(string phrase)
+        {
+            this._phrase = phrase;
+        }
+
+
+        public bool IsValid
+        {
+            get
+            {
+                var words = _phrase.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                return !ContainsDuplicates(words);
+            }
+        }
+
+        private static bool ContainsDuplicates(IEnumerable<string> words)
+        {
+            return words.GroupBy(word => word).Any(group => @group.Count() > 1);
+        }
+
+        public bool IsSecure
+        {
+            get
+            {
+                var words = _phrase.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(str => new string(str.OrderBy(c => c).ToArray()));
+                return !ContainsDuplicates(words);
+            }
+        }
+    }
+}
