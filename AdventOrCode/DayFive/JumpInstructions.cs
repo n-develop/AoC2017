07652eb56736e2403169371083d870035cac13e5
Lars Richter <github@n-develop.com>
2017-12-05 14:37:54 +0100

    Added Day 5 solution

---
+++ AdventOrCode/DayFive/JumpInstructions.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode.DayFive
+{
+    public class JumpInstructions
+    {
+        public static int GetStepsSecondStage(int[] instructions)
+        {
+            var currentIndex = 0;
+            var steps = 0;
+            while (currentIndex >= 0 && currentIndex < instructions.Length)
+            {
+                var offset = instructions[currentIndex];
+                if (offset >= 3)
+                {
+                    instructions[currentIndex] -= 1;
+                }
+                else
+                {
+                    instructions[currentIndex] += 1;
+                }
+                currentIndex += offset;
+                steps++;
+            }
+            return steps;
+        }
+
+        public static int GetSteps(int[] instructions)
+        {
+            var currentIndex = 0;
+            var steps = 0;
+            while (currentIndex >= 0 && currentIndex < instructions.Length)
+            {
+                var offset = instructions[currentIndex];
+                instructions[currentIndex] += 1;
+                currentIndex += offset;
+                steps++;
+            }
+            return steps;
+        }
+    }
+}
